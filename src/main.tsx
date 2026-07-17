8f4b7bf085f71934803103a4b22601778deaf5b9
derickferkxkd-collab <derickferkxkd@gmail.com>
2026-07-17 16:19:38 -0400

    Eliminar main.tsx

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AuthScreen from "./components/AuthScreen";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthScreen />
-  </React.StrictMode>
-);
